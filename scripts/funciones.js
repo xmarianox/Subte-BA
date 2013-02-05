881c4b643780c80cb98f0c6a60015a41f932f2a6
Mariano <xsin.crueldadx@gmail,com>
2013-02-05 11:51:57 -0300

    JQuery Mobile funcionando
    Template de jquery mobile para la app de subtes

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,21 +1,5 @@
-$(document).ready(function(){
-/*===== Función para Tabs  =====*/
-	$( "#tabs" ).tabs();
-/*===== Función para desplegrables =====*/
-	var accordion = $('.accordion h2');
-		$(accordion).click(function() {
-		$(accordion).removeClass("selected");
-		$(accordion).next().slideUp();
-		$(this).addClass("selected");
-		$(this).next().slideDown();
-	});
-/*===== BTN para cerrar desplegables =====*/
-	var cerrar = $('.barrios .cerrar');
-		$(cerrar).click(function() {
-		$(accordion).removeClass("selected");
-		$(accordion).next().slideUp();
-	});
-	
-	
-	
+/*===== JQuery Mobile =====*/
+$(document).bind("mobileinit", function(){
+  //$.mobile.foo = bar;
+
 });
